e54f3fbd25c38d1f83a5f4717ffe8c5ca101b091
Christopher L Plaisier, PhD <cplaisier@systemsbiology.org>
2013-01-15 15:50:00 -0800

    Removed bad file.

--- bulidTFBSDB.py
+++ bulidTFBSDB.py
@@ -1,3 +1,39 @@
+#################################################################
+# @Program: buildTFBSDB.py                                      #
+# @Version: 1                                                   #
+# @Author: Christopher L Plaisier, PhD                          #
+# @Sponsored by:                                                #
+# Nitin Baliga, ISB                                             #
+# Institute for Systems Biology                                 #
+# 401 Terry Ave North                                           #
+# Seattle, Washington  98109-5234                               #
+# (216) 732-2139                                                #
+# @Also Sponsored by:                                           #
+# Luxembourg Systems Biology Grant                              #
+# American Cancer Society Postdoctoral Fellowship               #
+#                                                               #
+# If this program is used in your analysis please mention who   #
+# built it. Thanks. :-)                                         #
+#                                                               #
+# Copyrighted by Chris Plaisier  1/15/2013                      #
+#################################################################
+
+###############
+### IMPORTS ###
+###############
+from pssm import pssm
+import cPickle, gzip, os, sys, re, os, math, shutil
+from copy import deepcopy
+from subprocess import *
+from random import sample
+from multiprocessing import Pool, cpu_count, Manager
+
+###
+# 
+###
+
+
+
 prev = ['chr0',0,0]
 chrs = {}
 inFile = open('combined.fps','r')
