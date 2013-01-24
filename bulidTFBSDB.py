223f6eba96fee21d25dd46b72735ec2a962a3d02
Christopher L Plaisier, PhD <cplaisier@systemsbiology.org>
2013-01-24 08:59:24 -0800

    Fixing up multiprocessing.

--- bulidTFBSDB.py
+++ bulidTFBSDB.py
@@ -27,50 +27,51 @@ from copy import deepcopy
 from subprocess import *
 from random import sample
 from multiprocessing import Pool, cpu_count, Manager
+import time
+
+mgr = Manager()
+
+## Multipocessing function for FIMO
+def runFimo(motif, seqFile='footprintSeqs/test.fasta'):
+#def runFimo(motif, seqFile='footprintSeqs/footprintSequences.fasta'):
+    outFile = open('tmp/pssm_'+motif.replace('$','_')+'.meme','w')
+    # Header crap
+    memeHeader = ''
+    memeHeader += 'MEME version 3.0\n\n'
+    memeHeader += 'ALPHABET= ACGT\n\n'
+    # Here is where we tell it what strand: for miRNAs this would just be '+'
+    memeHeader += 'strands: + -\n\n'
+    memeHeader += 'Background letter frequencies (from dataset with add-one prior applied):\n'
+    memeHeader += 'A 0.250 C 0.250 G 0.250 T 0.250\n\n'
+    outFile.write(memeHeader)
+    outFile.write(writeMotifs[motif].getMemeFormatted())
+    outFile.close()
+    motifHits[motif] = fimo(queryFile='tmp/pssm_'+motif.replace('$','_')+'.meme', seqFile=seqFile)
 
 ## Run FIMO on specified files
-def fimo(queryFile=None, seqFile=None, numSeqs=None):
-    fimoArgs = '--text --verbosity 1 --bgfile bgFile.meme '
-    if not numSeqs==None:
-        fimoArgs += '--output-pthresh 0.05 ' #+str(float(0.05)/float(numSeqs))+' '
+def fimo(queryFile=None, seqFile=None):
+    fimoArgs = '--max-stored-scores 1000000 --verbosity 4 --bgfile bgFile.meme -text --thresh 1e-5 '
     fimoArgs += str(queryFile)+' '+str(seqFile)
     print fimoArgs
-    #errOut = open('tmp/meme/stderr.out','w')
-    fimoProc = Popen("~/bin/fimo " + fimoArgs, shell=True,stdout=PIPE) #,stderr=errOut)
+    errOut = open('stderr.out','w')
+    fimoProc = Popen("~/bin/fimo " + fimoArgs, shell=True,stdout=PIPE,stderr=errOut)
+    errOut.close()
     output = fimoProc.communicate()[0].split('\n')
     #print output
     # Write out to a file
-    outFile = open('out.txt','w')
-    outFile.write('\n'.join(output))
-    outFile.close()
+    #outFile = open('out.txt','w')
+    #outFile.write('\n'.join(output))
+    #outFile.close()
     # Return results like cMonkey
-    res1 = {}
-    #print output.pop(0)
-    # Motif	Seq	Start	Stop	Log-odds	p-value	Site
+    res1 = []
+    print output.pop(0)
+    #pattern name	sequence name	start	stop	strand	score	p-value	q-value	matched sequence
     for line1 in output:
         splitUp = line1.split('\t')
         #print splitUp
-        if len(splitUp)==8:
-            if not splitUp[1] in res1:
-                res1[splitUp[1]] = { str(splitUp[2])+'_'+str(splitUp[3]):{ 'orientation':splitUp[0][0], 'start':splitUp[2], 'stop':splitUp[3], 'strand':splitUp[4], 'logOdds':float(splitUp[5]), 'pValue': float(splitUp[6]), 'site': splitUp[7] } }
-            else:
-                res1[splitUp[1]][str(splitUp[2])+'_'+str(splitUp[3])] = { 'orientation':splitUp[0][0], 'start':splitUp[2], 'stop':splitUp[3], 'strand':splitUp[4], 'logOdds':float(splitUp[5]), 'pValue': float(splitUp[6]), 'site': splitUp[7] }
-    res2 = {}
-    for gene in res1:
-        for hit in res1[gene]:
-            if not gene in res2:
-                res2[gene] = res1[gene][hit]['pValue']
-            elif res2[gene]>=res1[gene][hit]['pValue']:
-                res2[gene] = res1[gene][hit]['pValue']
-    #print res2
-    if len(res2)>0:
-        #res3 = benjaminiHochberg(res2, tests=numSeqs, alpha=0.001)
-        res3 = bonferroni(res2, tests=numSeqs, alpha=0.05)
-        print len(res2), len(res3)
-        return [[i,str(res2[i])] for i in res3]
-    else:
-        return []
-
+        if len(splitUp)==9:
+            res1.append({'chr': splitUp[1], 'start': splitUp[2], 'stop': splitUp[3], 'strand': splitUp[4], 'score':splitUp[5], 'p.value':float(splitUp[6]), 'match.sequence':splitUp[8]})
+    return res1
 
 #########################################################
 # Load up motifs and turn them into MEME formatted file #
@@ -82,33 +83,37 @@ def fimo(queryFile=None, seqFile=None, numSeqs=None):
 
 # TransFac
 pklFile = open('PSSMs/transfac_2012.1_PSSMs.pkl','rb')
-transfac = pklFile.load()
+transfac = cPickle.load(pklFile)
 pklFile.close()
 
 # UW Novel Motifs
 pklFile = open('PSSMs/novelPSSMsUW.pkl','rb')
-novel = pklFile.load()
+novel = cPickle.load(pklFile)
 pklFile.close()
 
-writeMotifs = dict(transfac, **novel)
+writeMotifs = mgr.dict(transfac, **novel)
+#writeMotifs = dict(novel)
+del novel
+del transfac
 
-# Run analysis
+# Prep for run
 if not os.path.exists('tmp'):
     os.mkdir('tmp')
-for motif in writeMotifs:
-    outFile = open('tmp/pssm_'+motif[0]+'.meme','w')
-    # Header crap
-    memeHeader = ''
-    memeHeader += 'MEME version 3.0\n\n'
-    memeHeader += 'ALPHABET= ACGT\n\n'
-    # Here is where we tell it what strand: for miRNAs this would just be '+'
-    memeHeader += 'strands: + -\n\n'
-    memeHeader += 'Background letter frequencies (from dataset with add-one prior applied):\n'
-    memeHeader += 'A 0.250 C 0.250 G 0.250 T 0.250\n\n'
-    outFile.write(memeHeader)
-    outFile.write(transfacPssms[motif[0]].getMemeFormatted())
-    outFile.close()
-    # Run fimo to get the target sites
-    res1 = fimo(queryFile='tmp/pssm_'+motif[0]+'.meme', seqFile='tmp/meme/fasta/eQTL_'+str(motif[1]).replace('_','.')+'.fasta',numSeqs=float(100))
-    break
+motifHits = mgr.dict()
+
+# Run fimo to get the target sites
+print 'Running Weeder...'
+cpus = cpu_count()
+print 'There are', cpus,'CPUs avialable.'
+pool = Pool(processes=cpus)
+#runFimo(writeMotifs.keys()[0])
+pool.map(runFimo,writeMotifs.keys()[0:8])
+
+
+#t1 = time.time()
+#res1 = fimo(queryFile='tmp/pssm_'+motif.replace('$','_')+'.meme', seqFile='footprintSeqs/test.fasta')
+#res1 = fimo(queryFile='tmp/pssm_'+motif.replace('$','_')+'.meme', seqFile='footprintSeqs/footprintSequences.fasta', numSeqs=float(100))
+#t2 = time.time()
+#print 'Took %0.3f s' % (t2-t1)
+#break
 
